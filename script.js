c938dbb49f42ed8e40e49c701fa94fea4ddce7e9
batoolZeyad <batoolzeyad55@gmail.com>
2026-04-15 10:03:49 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,7 @@
 function generatePDF() {
   const { jsPDF } = window.jspdf;
-  const doc = new jsPDF('p','mm','a4');
-
+  const doc = new jsPDF();
   doc.setFontSize(14);
   doc.text("Online Learning Observation Form", 105, 15, null, null, "center");
-
-  doc.setFontSize(10);
-  doc.text("Teacher: " + teacherName.value, 10, 30);
-  doc.text("Observer: " + observerName.value, 10, 36);
-  doc.text("Date: " + date.value, 10, 42);
-
-  doc.text("Final Comments:", 10, 52);
-  doc.text(finalComments.value || "-", 10, 58, {maxWidth:180});
-
   doc.save("Observation_Form.pdf");
 }
